531827f07d2d61e75dff51080eef34141ccb17dd
Marcelo Roberto <contato@marceloroberto.com.br>
2025-12-16 12:24:40 -0300

    Change download path for update.sh script

--- copyupdate.sh
+++ copyupdate.sh
@@ -1,1 +1,1 @@
-wget -O ~/usr/local/bin/update.sh https://raw.githubusercontent.com/marceloroberto/cbmmm/main/update.sh && chmod +x ~/usr/local/bin/update.sh
+wget -O /usr/local/bin/update.sh https://raw.githubusercontent.com/marceloroberto/cbmmm/main/update.sh && chmod +x /usr/local/bin/update.sh
